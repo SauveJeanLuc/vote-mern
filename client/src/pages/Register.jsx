0b16792560aa86031b68d4bf5b116a79611b8fc6
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-01 23:43:42 +0200

    register iintegration

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+
+function Register() {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function registerUser(event) {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:3030/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password
+      }),
+    }) 
+    const data = await response.json();
+
+    console.log(data);
+
+    if(data.status === 'ok') {
+      window.location.href = '/login'
+    }
+
+  }
+  
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={registerUser}>
+        <input
+         value={name}
+         onChange={function(e){
+          setName(e.target.value);
+         }}
+         type="text" 
+         placeholder='Name'
+        />
+        <input
+         value={email}
+         onChange={function(e){
+          setEmail(e.target.value);
+         }}        
+         type="email" 
+         placeholder='Last Name' 
+        />
+        <input 
+          value={password}
+          onChange={function(e){
+            setPassword(e.target.value);
+           }}          
+          type={password} 
+          placeholder='Password' />
+        
+        <input type="submit" value="Register"/>
+      </form>
+    </div>
+  )
+}
+
+export default Register;
