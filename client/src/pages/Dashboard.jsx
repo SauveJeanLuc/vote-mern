b505db4937522095e5b039d9bb211d8a801ad117
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-01 23:42:53 +0200

    basic Dashboard impl

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,95 @@
+import { useEffect } from "react";
+// import {jwt} from "jsonwebtoken"
+import jwt_decode from "jwt-decode";
+import { useState } from "react";
+ 
+
+
+export default function Dashboard  () {
+
+   const [quote, setQuote] = useState('')
+   const [tempQuote, setTempQuote] = useState('')
+
+   async function populateQuote(){
+
+      const req = await fetch('http://localhost:3030/api/quote', {
+         method: 'GET',
+         headers: {
+            'x-access-token': localStorage.getItem('token')
+         },       
+      })
+
+      const data = await req.json()
+
+      if(data.status === 'ok'){
+         setQuote(data.quote)
+      }else {
+         alert("Error 1: " + data.error )
+      }
+      console.log(data)
+   }
+
+   // Verify if user is logged in
+    
+   useEffect(() => {
+      const token = localStorage.getItem('token');
+      if (token) {
+
+         const user = jwt_decode(token);
+
+         if(!user) {
+            localStorage.removeItem('token')
+            window.location.href = '/login'
+         } else {
+            populateQuote();
+         }
+      } else{
+         window.location.href = '/login'
+      }
+   }, [])
+
+   async function updateQuote (event){
+
+      event.preventDefault()
+       
+      const req = await fetch('http://localhost:3030/api/quote', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': localStorage.getItem('token')
+         },       
+         body: JSON.stringify({
+            quote: tempQuote,
+         })
+      })
+      
+      const data = await req.json()
+      if(data.status === 'ok'){
+         setQuote(tempQuote)
+         setTempQuote('')
+      }else {
+         alert("Error 2 : " + data.error)
+      }
+      
+   }
+
+   return (
+      <div>
+         <h1>Your quote: {quote || "No quote found "}</h1> 
+         <form onSubmit={updateQuote}>
+            <input 
+               type="text" 
+               placeholder="Quote"
+               value={tempQuote}
+               onChange={(e) => setTempQuote(e.target.value)}
+            />
+            <input type="submit" value=" Update Quote "/>
+         </form>
+         <form onSubmit={populateQuote}>
+         
+            <input type="submit" value="Force Fetch "/>
+         </form>
+      </div>
+   )
+   
+}
