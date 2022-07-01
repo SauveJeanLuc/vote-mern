d05f226dbce67bf6087d6331ea25d6a1a4222d79
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-01 23:42:38 +0200

    Browser route in root

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
   </React.StrictMode>
 )
