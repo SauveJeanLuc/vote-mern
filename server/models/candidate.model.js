32c3de98e351719e727f50f51512e9b99f1d61aa
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-02 11:37:04 +0200

    candidate model

---
+++ server/models/candidate.model.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Joi = require("joi")
+
+const Candidate = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        secondName: {
+            type: String,
+            required: true,
+        },
+        profileUrl: {
+            type: String,
+            required: true
+        },
+        nbrOfVotes: {
+            type: Number,
+            default: 0
+        }
+    }
+)
+
+const model = mongoose.model("Candidate", Candidate)
+
+function validateCandidate(candidate) {
+    const schema = Joi.object({
+        firstName: Joi.string().min(3).max(250).required(),
+        secondName: Joi.string().min(3).max(250).required(),
+        profileUrl: Joi.string().required(),
+    });
+
+    return schema.validate(candidate);
+}
+
+module.exports.Candidate = model;
+module.exports.validateCandidate = validateCandidate;
+
