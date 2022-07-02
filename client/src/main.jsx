2602ef9e4d453f8fbfb3c67b65725c2f82575ae3
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-02 20:09:34 +0200

    refine main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-// import Login from './pages/auth/LogIn'
-// import Register from './pages/auth/Register'
-// import Admin from './pages/dashboards/Admin'
-// import NavigationBar from './components/NavigationBar'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
