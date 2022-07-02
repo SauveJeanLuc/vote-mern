f0b98b8dcb7e09a43e89fb9f93ead1c8817b7abf
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-02 11:39:17 +0200

    refined user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,15 +1,48 @@
+const Joi = require('joi')
+const { joiPassword } = require('joi-password')
 const mongoose = require('mongoose')
+
+
 
 const User = new mongoose.Schema(
     {
-        name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true },
-        quote: { type: String},
-    }, 
-    { collection: 'user_data'}
+        name: { 
+            type: String, 
+            required: true 
+        },
+        email: { 
+            type: String, 
+            required: true, 
+            unique: true 
+        },
+        password: { 
+            type: String, 
+            required: true 
+        },
+        candidateId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Candidate",
+            default: null,
+        }
+    }
+    // { collection: 'user_data'}
 )
 
-const model = mongoose.model('UserData',User )
+function validateUser(user){
+    const schema = Joi.Object(
+        {
+            name: Joi.string().required(),
+            email: Joi.string().email(),
+            password: joiPassword
+            .string()
+            .minOfSpecialCharacters(1)
+            .minOfUppercase(1)
+        }
+    )
+    
+    return schema.validate(user)
+}
+
+const model = mongoose.model('User',User )
 
 module.exports = model
