2f3ab2c1a36452a51efe43c345008a7f29a9db3b
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-02 20:07:59 +0200

    pre - include styles in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+// import Login from "./pages/auth/LogIn";
+// import Register from "./pages/auth/Register";
+// import Admin from "./pages/dashboards/Admin";
+// import Candidate from "./pages/dashboards/Candidates";
+
 
 
 export default function App() {
@@ -14,6 +19,14 @@ export default function App() {
                     <Route exact path="/register" element={<Register/>}></Route>
                     <Route exact path="/dashboard" element={<Dashboard/>}></Route>
                 </Routes>
+
+                // <Routes>
+                //     <Route exact path="/"  element={<Login/>}></Route>
+                //     <Route exact path="/login"  element={<Login/>}></Route>
+                //     <Route exact path="/register" element={<Register/>}></Route>
+                //     <Route exact path="/admin" element={<Admin/>}></Route>
+                //     <Route exact path="/candidate" element={<Candidate/>}></Route>
+                // </Routes>
             // {/* <span>Me Trying</span> */}
         // </div>
     );
