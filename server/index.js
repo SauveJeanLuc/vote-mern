6efbdb1a309a8d16c0238c08e624969c8b0d12e5
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-02 11:36:40 +0200

    refine index

--- server/index.js
+++ server/index.js
@@ -2,125 +2,26 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const User = require('./models/user.model')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const userRoutes = require('./routes/user.route')
+const candidateRoutes = require('./routes/candidate.route')
 
 app.use(cors()) // Handle CORS
 app.use(express.json()) // Parse Everything that comes as req.body json
-mongoose.connect('mongodb://localhost:27017/voting-mern') // Connect to MongoDB
 
-app.post('/api/register', async (req, res)=>{
-    console.log(req.body)
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/voting-mern')
+.then( () => console.log ("Connected to DB" )) 
+.catch( (err) => console.error("Could not connect to MongoDB"))
 
-    // Store hashed password
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+// User Routes
+app.use('/api/user', userRoutes);
 
-    try{    
-        const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
-        })
+// Candidate Routes
+app.use('/api/candidate', candidateRoutes);
 
-        res.json({
-            status : 'ok',
-            data: user,
-        })
-        // res.send(user)
-    } catch (err){
-        res.json({
-            status : 'error',
-            err: err
-        })
-    }
-
-})
-
-app.post('/api/login', async (req, res) => {
-    try{
-        const user = await User.findOne({
-            email: req.body.email,
-            password: req.body.password,
-        })
-
-        // Check If password is valid compared to hash
-
-        if(!user) {
-            return res.json({ status: 'error', error: 'Invalid login'})
-        }
-
-        const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
-        
-        if(isPasswordValid) {
-
-            const token = jwt.sign({
-                name: user.name,
-                email: user.email,
-            }, 'secret123')
-
-            return res.json({status: 'ok', user: true, token: token})
-
-        } else {
-
-            return res.json({status: 'err', user: false})
-
-        }
-
-    }
-    catch(err){
-        res.json({
-            status: 'error',
-            err: err
-        })
-    }
-})
-
-app.get('/api/quote', async (req, res) => {
-
-    try {
-        const token = req.headers['x-access-token'];
-        const decoded = jwt.verify(token, 'secret123')
-        const email = decoded.email
-        const user = await User.findOne(
-            { email: email}
-        )
-        
-        res.json( { status: 'ok', quote: user.quote} )
-
-    } catch(error){
-        console.log(error)
-        res.json({status: 'error', error: 'invalid token'})
-    }
-
-})
-
-app.post('/api/quote', async (req, res) => {
-
-    try {
-        console.log('POST was called')
-        const token = req.headers['x-access-token'];
-        const decoded = jwt.verify(token, 'secret123')
-        const email = decoded.email
-        console.log('Decoded Email:' + email)
-        console.log('Sent Quote:' + req.body.quote)
-
-        await User.updateOne(
-            { email: email}, 
-            { $set : { quote: req.body.quote}}
-        )
-        
-        return { status: 'ok'}
-
-    } catch(error){
-        console.log(error)
-        res.json({status: 'error', error: 'invalid token'})
-    }
-
-})
-
-app.get('/hello', (req,res) => {
-    res.send('hello world');
+// Welcome Route
+app.get('/', (req,res) => {
+    res.send('VOTING SYSTEM ࡙࡙࡙࡙࡙');
 })
 
 app.listen(3030, () => {
