506543762b78180fcc5da8238450d93d4493431a
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-02 20:09:19 +0200

    refine main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+// import Login from './pages/auth/LogIn'
+// import Register from './pages/auth/Register'
+// import Admin from './pages/dashboards/Admin'
+// import NavigationBar from './components/NavigationBar'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
