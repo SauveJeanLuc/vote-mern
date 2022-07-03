ada4fa4bdd00838b6ae7b265f7a3ecf35283d0fd
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-03 02:11:09 +0200

    clean app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-// import Login from "./pages/auth/LogIn";
-// import Register from "./pages/auth/Register";
-// import Admin from "./pages/dashboards/Admin";
-// import Candidate from "./pages/dashboards/Candidates";
+import Login from "./pages/auth/LogIn";
+import Register from "./pages/auth/Register";
+import Admin from "./pages/dashboards/Admin";
+import Candidate from "./pages/dashboards/Candidates";
 
 
 
 export default function App() {
     return (
-        // <div>
                 <Routes>
                     <Route exact path="/"  element={<Login/>}></Route>
                     <Route exact path="/login"  element={<Login/>}></Route>
                     <Route exact path="/register" element={<Register/>}></Route>
-                    <Route exact path="/dashboard" element={<Dashboard/>}></Route>
+                    <Route exact path="/admin" element={<Admin/>}></Route>
+                    <Route exact path="/candidate" element={<Candidate/>}></Route>
                 </Routes>
-
-                // <Routes>
-                //     <Route exact path="/"  element={<Login/>}></Route>
-                //     <Route exact path="/login"  element={<Login/>}></Route>
-                //     <Route exact path="/register" element={<Register/>}></Route>
-                //     <Route exact path="/admin" element={<Admin/>}></Route>
-                //     <Route exact path="/candidate" element={<Candidate/>}></Route>
-                // </Routes>
-            // {/* <span>Me Trying</span> */}
-        // </div>
     );
 }
 
